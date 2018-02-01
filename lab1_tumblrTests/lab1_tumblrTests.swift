a84d1e53e7dafd4c3f296b6072c126ebb26aca94
Yiyuan Ma <yim096@ucsd.edu>
2018-01-31 20:10:04 -0800

    Initial Commit

---
+++ lab1_tumblrTests/lab1_tumblrTests.swift
@@ -0,0 +1,36 @@
+//
+//  lab1_tumblrTests.swift
+//  lab1_tumblrTests
+//
+//  Created by 马懿元 on 1/31/18.
+//  Copyright © 2018 Sparrow. All rights reserved.
+//
+
+import XCTest
+@testable import lab1_tumblr
+
+class lab1_tumblrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
